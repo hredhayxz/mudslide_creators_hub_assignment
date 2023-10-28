7a07d8bfeee8024556c963fd85dec17b78bc079e
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-10-28 14:43:31 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mudslide_creators_hub_assignment
+ Mudslide Creators Hub UI assignment
---
+++ android/app/src/main/kotlin/com/example/mudslide_creators_hub_assignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mudslide_creators_hub_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
