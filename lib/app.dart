90574a46d7370f08111b60a545570e658690e079
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-10-28 21:50:35 +0600

    Profile design in progress

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mudslide_creators_hub_assignment/presentation/ui/screens/friends_screen.dart';
 import 'package:mudslide_creators_hub_assignment/presentation/ui/screens/passions_screen.dart';
+import 'package:mudslide_creators_hub_assignment/presentation/ui/screens/profile_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       //   useMaterial3: true,
       // ),
-      home: FriendsScreen(),
+      home: ProfileScreen(),
     );
   }
 }
---
+++ lib/presentation/ui/screens/profile_screen.dart
@@ -0,0 +1,218 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: 2000,
+            child: Stack(
+              children: [
+                //SizedBox(height: 2000,),
+                // SvgPicture.asset(
+                //   'assets/images/people.svg',
+                //   width: 200,
+                //   height: 200,
+                // ),
+                Image.asset(
+                  'assets/images/photo_main.png',
+                  width: screenSize.width,
+                  fit: BoxFit.contain,
+                ),
+                Positioned(
+                  top: 350,
+                  child: Container(
+                    height: 1000,
+                    width: screenSize.width,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 50,),
+                          Row(
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Jessica Parker, 23',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleLarge
+                                        ?.copyWith(fontSize: 30),
+                                  ),
+                                  const Text(
+                                    'Professional model',
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                                    border: Border.all(
+                                      color: Colors.grey.shade300,
+                                      width: 1.0,
+                                    ),
+                                  ),
+                                  child: const Icon(
+                                    Icons.send_outlined,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'Location',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleLarge
+                                        ?.copyWith(fontSize: 22),
+                                  ),
+                                  const Spacer(),
+                                  InkWell(
+                                    onTap: () {},
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.red.shade50,
+                                        borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                                      ),
+                                      child: const Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Row(
+                                          children: [
+                                            Icon(
+                                              Icons.location_on_outlined,
+                                              color: Colors.red,
+                                            ),
+                                            Text('1 km')
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Text(
+                                'Professional model',
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'About',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleLarge
+                                    ?.copyWith(fontSize: 22),
+                              ),
+                              const Text(
+                                'My name is Jessica Parker and I enjoyed meeting new people and finding ways to help them have an uplifting. I enjoyed reading..',
+                              ),
+                              InkWell(
+                                onTap: (){},
+                                child: const Text(
+                                  'Read more',
+                                  style: TextStyle(color: Colors.red, fontSize: 17,fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20,),
+                          
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 307.5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 70.0,
+                        height: 70.0,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey,
+                        ),
+                        child: const Icon(
+                          Icons.cancel_outlined,
+                          color: Colors.amber,
+                          size: 35.0, // Adjust the size as needed
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        width: 85.0,
+                        height: 85.0,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                        child: const Icon(
+                          Icons.favorite,
+                          color: Colors.white,
+                          size: 40.0,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        width: 70.0,
+                        height: 70.0,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey,
+                        ),
+                        child: const Icon(
+                          Icons.star,
+                          color: Colors.deepPurple,
+                          size: 35.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
